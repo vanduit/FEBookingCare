de695e46a4a69d0ac6768d4f92dd8ebcf8e83130
POUCHENVN\web.vandu <web.vandu@pyv.com.vn>
2023-11-25 10:53:20 +0700

    Update

--- src/containers/Patient/Doctor/DetailDoctor.js
+++ src/containers/Patient/Doctor/DetailDoctor.js
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 
 class DetailDoctor extends Component {
     render() {
-
+        console.log('check new :', this.props.params.id);
         return (
             <div>
                 Detail Doctor
