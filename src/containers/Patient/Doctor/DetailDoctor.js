16d82e60b6a40d6bb5f2dd93182cbd9be0e3d394
POUCHENVN\web.vandu <web.vandu@pyv.com.vn>
2023-12-18 14:25:45 +0700

    Update

--- src/containers/Patient/Doctor/DetailDoctor.js
+++ src/containers/Patient/Doctor/DetailDoctor.js
@@ -3,13 +3,14 @@ import { connect } from "react-redux";
 import HomeHeader from '../../HomePage/HomeHeader';
 import './DetailDoctor.scss';
 import {getDetailInforDoctor} from '../../../services/userService';
+import { LANGUAGES } from '../../../utils';
 
 class DetailDoctor extends Component {
 
     constructor(props){
         super(props);
         this.state={
-
+            detailDoctor:{}
         }
     }
 
@@ -17,7 +18,11 @@ class DetailDoctor extends Component {
         if(this.props.match && this.props.match.params && this.props.match.params.id){
             let id = this.props.match.params.id;
             let res = await getDetailInforDoctor(id);
-            console.log('check res :', res);
+            if(res && res.errCode == 0){
+                this.setState({
+                    detailDoctor: res.data
+                })
+            }
         }
     }
 
@@ -26,6 +31,14 @@ class DetailDoctor extends Component {
     }
 
     render() {
+        console.log('check state :', this.state);
+        let {language} = this.props;
+        let {detailDoctor} = this.state;
+        let nameVi = '', nameEn = '';
+        if(detailDoctor && detailDoctor.positionData){
+             nameVi = `${detailDoctor.positionData.valueVi},${detailDoctor.firstName} ${detailDoctor.lastName}`;
+             nameEn = `${detailDoctor.positionData.valueEn},${detailDoctor.firstName} ${detailDoctor.lastName}`;
+        }
         return (
             <>
                 <HomeHeader
@@ -33,15 +46,19 @@ class DetailDoctor extends Component {
                 />
                 <div className="doctor-detail-container">
                     <div className="intro-doctor">
-                        <div className="content-left">
+                        <div className="content-left" style={{ backgroundImage : `url(${detailDoctor && detailDoctor.image ? detailDoctor.image : ''})` }}>
 
                         </div>
                         <div className="content-right">
                             <div className="up">
-                                Phó giáo sư tiến sĩ     
+                                {language === LANGUAGES.VI ? nameVi : nameEn} 
                             </div>
                             <div className="down">
-                                abababaababababbabababbabababbababa
+                                {detailDoctor.Markdown && detailDoctor.Markdown.description 
+                                    && <span>
+                                        {detailDoctor.Markdown.description}
+                                    </span>
+                                }
                             </div>
                         </div>
                     </div>
@@ -49,7 +66,12 @@ class DetailDoctor extends Component {
 
                     </div>
                     <div className="detail-infor-doctor">
-
+                        {detailDoctor && detailDoctor.Markdown && detailDoctor.Markdown.contentHTML
+                            && 
+                            <div dangerouslySetInnerHTML={{__html: detailDoctor.Markdown.contentHTML }}>
+                                
+                            </div>
+                        }
                     </div>
                     <div className="comment-doctor">
 
@@ -62,7 +84,7 @@ class DetailDoctor extends Component {
 
 const mapStateToProps = state => {
     return {
-        
+        language: state.app.language,
     };
 };
 
